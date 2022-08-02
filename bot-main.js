395a2061b0f8809016593d6782c439f5710128bc
lunaJYJ <devmonarch0115@gmail.com>
2022-08-02 18:12:18 +0900

    feature : 트위터 멘션 확인

--- bot-main.js
+++ bot-main.js
@@ -1,5 +1,6 @@
 const { Client, GatewayIntentBits } = require("discord.js");
-const { token } = require("./setting_variable.json");
+const { tweet_api_bearer, token } = require("./setting_variable.json");
+var axios = require("axios");
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -9,15 +10,40 @@ client.once("ready", () => {
 
 client.on("interactionCreate", async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
-
-  const { commandName } = interaction;
+  const { commandName, options } = interaction;
 
   if (commandName === "ping") {
     await interaction.reply("Pong!");
-  } else if (commandName === "server") {
-    await interaction.reply("Server info.");
-  } else if (commandName === "user") {
-    await interaction.reply("User info.");
+  }
+
+  if (commandName === "tweet-give-coin") {
+    let tweetIds = options.get("link").value;
+    tweetIds = tweetIds.split("?")[0];
+    tweetIds = tweetIds.split("/");
+    tweetIds = tweetIds[tweetIds.length - 1];
+
+    try {
+      var config = {
+        method: "get",
+        url: `https://api.twitter.com/2/tweets/${tweetIds}`,
+        headers: {
+          "User-Agent": "v2TweetLookupJS",
+          authorization: `Bearer ${tweet_api_bearer}`,
+        },
+      };
+
+      const tweetResult = await axios(config);
+      const tweetResultData = tweetResult.data.data.text;
+
+      if (tweetResultData.indexOf("@CybergalzNFT") > -1) {
+        await interaction.reply("CyberGalz mentioned");
+      } else {
+        await interaction.reply("This tweet not contain Cybergalz mention");
+      }
+    } catch (e) {
+      console.log(e);
+      console.log("Get Tweet Error");
+    }
   }
 });
 
--- deploy-commands.js
+++ deploy-commands.js
@@ -1,11 +1,26 @@
-const { SlashCommandBuilder, Routes } = require("discord.js");
+const {
+  SlashCommandBuilder,
+  Routes,
+  SlashCommandStringOption,
+} = require("discord.js");
 const { REST } = require("@discordjs/rest");
-const { clientId, guildId, token } = require("./token.json");
+const { clientId, guildId, token } = require("./setting_variable.json");
 
 const commands = [
   new SlashCommandBuilder()
     .setName("ping")
     .setDescription("Replies with pong!"),
+  new SlashCommandBuilder()
+    .setName("tweet-give-coin")
+    .addStringOption(
+      new SlashCommandStringOption()
+        .setName("link")
+        .setDescription("Tweet link")
+        .setRequired(true)
+    )
+    .setDescription(
+      "Verification your tweet for confirm exactly mentioned @CyberGalz"
+    ),
 ].map((command) => command.toJSON());
 
 const rest = new REST({ version: "10" }).setToken(token);
