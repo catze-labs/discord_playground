e2cd45cedd54f9d3ba89fc5e7e7aacd0745aa17d
lunaJYJ <devmonarch0115@gmail.com>
2022-08-03 13:46:58 +0900

    init : nest serivce, contorller, base entity

--- src/bot/bot.module.ts
+++ src/bot/bot.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { BotController } from './bot.controller';
+import { BotService } from './bot.service';
 
-@Module({})
+@Module({
+  controllers: [BotController],
+  providers: [BotService]
+})
 export class BotModule {}
